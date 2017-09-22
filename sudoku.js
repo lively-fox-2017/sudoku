c1f095afcc792b88d8752d832969713666c696e4
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-22 14:53:09 +0700

    Commit Release 1, 2, 3

--- sudoku.js
+++ sudoku.js
@@ -12,6 +12,122 @@ class Sudoku {
     }
   }
 
+  rowCheck(x) {
+    let element = {
+      '1': 1,
+      '2': 1,
+      '3': 1,
+      '4': 1,
+      '5': 1,
+      '6': 1,
+      '7': 1,
+      '8': 1,
+      '9': 1,
+    }
+
+    for (let i = 0; i < 9; i++) {
+      element[this.boardELement[x][i]]--;
+      if (element[this.boardELement[x][i]] < 0)
+        return false;
+    }
+    return true;
+  }
+
+  colCheck(x) {
+    let element = {
+      '1': 1,
+      '2': 1,
+      '3': 1,
+      '4': 1,
+      '5': 1,
+      '6': 1,
+      '7': 1,
+      '8': 1,
+      '9': 1,
+    }
+
+    for (let i = 0; i < 9; i++) {
+      element[this.boardELement[i][x]]--;
+      if (element[this.boardELement[i][x]] < 0)
+        return false;
+    }
+    return true;
+  }
+
+  initWindow(window) {
+    let init = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
+
+    let baris = [];
+    let kolom = [];
+    // Choose Window
+    switch (window) {
+      case 0:
+        baris = init[0];
+        kolom = init[0];
+        break;
+      case 1:
+        baris = init[0];
+        kolom = init[1];
+        break;
+      case 2:
+        baris = init[0];
+        kolom = init[2];
+        break;
+      case 3:
+        baris = init[1];
+        kolom = init[0];
+        break;
+      case 4:
+        baris = init[1];
+        kolom = init[1];
+        break;
+      case 5:
+        baris = init[1];
+        kolom = init[2];
+        break;
+      case 6:
+        baris = init[2];
+        kolom = init[0];
+        break;
+      case 7:
+        baris = init[2];
+        kolom = init[1];
+        break;
+      case 8:
+        baris = init[2];
+        kolom = init[2];
+        break;
+    }
+    return [baris, kolom];
+  }
+
+  windowCheck(window) {
+    let element = {
+      '1': 1,
+      '2': 1,
+      '3': 1,
+      '4': 1,
+      '5': 1,
+      '6': 1,
+      '7': 1,
+      '8': 1,
+      '9': 1,
+    }
+    let areaWindow = this.initWindow(window);
+    for (let i = 0; i < areaWindow[0].length; i++) {
+      for (let j = 0; j < areaWindow[1].length; j++) {
+        element[this.boardELement[areaWindow[0][i]][areaWindow[0][j]]]--;
+        if (element[this.boardELement[areaWindow[0][i]][areaWindow[0][j]]] < 0)
+          return false;
+      }
+    }
+    return true;
+  }
+
   solve() {}
 
   // Returns a string representing the current state of the board
