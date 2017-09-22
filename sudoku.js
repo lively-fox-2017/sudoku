49adc9eb5802bb68ee0fc585bb9c5249c04799df
lokiloko <ianw.tkj2@gmail.com>
2017-09-22 14:18:16 +0700

    Printing board done

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,28 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.boardFill = board_string;
+    this.sudoBoard = [];
+    this.generateBoard();
+  }
+  generateBoard() {
+    for (var i = 1; i < 10; i++) {
+      var temp = [];
+      for (var j = 1; j < 10;j++){
+        temp.push([this.boardFill[(j * i)-1]]);
+      }
+      this.sudoBoard.push(temp);
+    }
+  }
+  solve() {
 
-  solve() {}
+  }
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    return this.sudoBoard;
+  }
 }
 
 // The file has newlines at the end of each line,
