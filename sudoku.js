e4ecfd773750f44e019978d7def5951921795001
aditReza <lete.note3@gmail.com>
2017-09-24 17:30:36 +0700

    masih release 4

--- sudoku.js
+++ sudoku.js
@@ -4,31 +4,73 @@ class Sudoku {
     constructor(board_string) {
         this.board_string = board_string
         this.layout = []
+        this.angkaNol = []
     }
 
     solve() {
+
+        /* solve release 4 */
         let inputAngka = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
         for (let x = 0; x < 9; x++) {
             for (let c = 0; c < 9; c++) {
-                console.log(this.layout[x][c])
-                if (this.layout[x][c] === 0) {
+                //console.log(this.layout[x][c])
+                if (this.layout[x][c] == 0) { // = number
                     for (let i = 0; i < inputAngka.length; i++) {
-                        console.log(this.rowCheck(x, inputAngka[i]))
-                        console.log(this.colCheck(c, inputAngka[i]))
-                        console.log(this.areacheck(x, c, inputAngka[i]))
+                        // console.log(this.rowCheck(x, inputAngka[i]))
+                        // console.log(this.colCheck(c, inputAngka[i]))
+                        // console.log(this.areacheck(x, c, inputAngka[i]))
                         if (this.rowCheck(x, inputAngka[i]) && this.colCheck(c, inputAngka[i]) && this.areacheck(x, c, inputAngka[i])) {
                             this.layout[x][c] = inputAngka[i]
                         }
-                        // if(this.rowCheck(r, inputAngka[i]) && this.colCheck(c, inputAngka[i]) && this.areaCheck(r, c, inputAngka[i])) {
-                        //     this.layout[r][c] = inputAngka[i].toString()
-                        // }
+
                     }
                 }
             }
         }
-        console.log(this.layout)
+        //console.log(this.layout)
         return this.layout
+
+        /*release 5 masih error */
+        // this.ngecekNol()
+        // let batas = 9,
+        //     row, col, nilai, ditemukan;
+        // console.log(this.angkaNol.length)
+        // for (var i = 0; i < this.angkaNol.length;) {
+        //     row = this.angkaNol[i][0]
+        //     col = this.angkaNol[i][1]
+        //     nilai = Number(this.layout[row][col]) + 1
+        //     ditemukan = false
+        //     console.log(row)
+        //     while (!ditemukan && nilai <= batas) {
+        //         if (this.rowCheck(row, nilai) && this.colCheck(col, nilai) && this.areacheck(row, col, nilai)) {
+        //             ditemukan = true
+        //             this.layout[row][col] = nilai.toString()
+        //             i++
+        //         } else {
+        //             nilai++ // jika ga ada nyari nilai selanjutnya
+        //         }
+        //     }
+
+        //     if (!ditemukan) {
+        //         this.layout[row][col] = 0
+        //         i--
+        //     }
+        // }
+        // return this.layout
+    }
+
+    ngecekNol() { // cek nol > push // berdasarkan index row col
+        console.log(this.layout)
+        for (var i = 0; i < this.layout.length; i++) {
+            for (var j = 0; j < this.layout.length; j++) {
+                if (this.layout[i][j] === '0') {
+                    this.angkaNol.push([i, j])
+                }
+            }
+        }
+        //console.log(this.angkaNol)
+        return this.angkaNol
     }
 
     // Returns a string representing the current state of the board
@@ -36,10 +78,11 @@ class Sudoku {
     layout9x9() {
         // layout 9x9
         let newArr = this.board_string.split('')
-        console.log(newArr)
-        for (let i = 0; i < newArr.length; i++) {
+            //console.log(newArr)
+        for (let i = 0; i < 9; i++) {
             this.layout.push(newArr.splice(0, 9))
         }
+        //console.log(this.layout)
         return this.layout
     }
 
@@ -70,20 +113,6 @@ class Sudoku {
         let row = this.layout[rowIndex]
             //console.log((row.indexOf(searchRow.toString())===-1))
         return (row.indexOf(searchRow.toString()) === -1) ? true : false
-            //this.layout9x9()
-        console.log(row)
-            // let rowKe = row[rowIndex - 1]
-            // console.log(rowKe)
-            // let counter = 0
-
-        // for (let j = 0; j < rowKe.length; j++) {
-        //     console.log(rowKe[j])
-        //     if (rowKe[j].indexOf(searchRow) !== -1) {
-        //         counter++
-        //     }
-        // }
-        // console.log(counter)
-        // return (counter > 0) ? false : true
     }
 
     colCheck(colIndex, searchCol) {
@@ -95,30 +124,20 @@ class Sudoku {
         }
 
         return (col.indexOf(searchCol.toString()) === -1) ? true : false
-            // let counter = 0
-            // console.log(col)
-            // for (let j = 0; j < col.length; j++) {
-            //     console.log(col[j])
-            //     if (col[j].indexOf(searchCol) !== -1) {
-            //         counter++
-            //     }
-            // }
-            // console.log(counter)
-            // return (counter > 0) ? false : true
     }
 
 
     areacheck(row, col, search) {
         // area 3x3
         let area51 = ''
-        console.log(search)
+            //console.log(search)
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 //console.log(this.layout[(i + row) - (row % 3)][(j + col) - (col % 3)])
                 area51 += this.layout[(i + row) - (row % 3)][(j + col) - (col % 3)]
             }
         }
-        console.log(area51)
+        //console.log(area51)
 
         // cek area 3x3
         return (area51.indexOf(search.toString()) === -1) ? true : false
@@ -153,14 +172,15 @@ game.colCheck(2, 4)
 game.rowCheck(1, 4)
 game.areacheck(1, 1, 8)
 game.layout9x9()
-game.board()
+game.ngecekNol()
 game.solve()
+game.board()
 
 
-console.log(game.rowCheck(1, 4))
-console.log(game.colCheck(1, 5))
-console.log(game.areacheck(0, 0, 1))
-    //console.log(game.solve())
 
+// console.log(game.rowCheck(1, 4))
+// console.log(game.colCheck(1, 5))
+// console.log(game.areacheck(0, 0, 8))
+// console.log(game.ngecekNol())
 console.log(game.board())
     //console.log(game.solve())
